1e0ac580c3e18bc6a449a739852739eee4be106c
AdhritShetty <adhritshetty2009@gmail.com>
2025-04-01 15:56:38 +0000

    And and or

---
+++ And and or operator.py
@@ -0,0 +1,13 @@
+a=10
+b=-10
+c=0
+
+if a>0 and b>0 :
+    print("they have a true boolean value")
+else:
+    print("they have a false boolean value")
+
+if b>0 or c>0 :
+    print("they are greater than zero")
+else:
+    print("they are less than zero")
